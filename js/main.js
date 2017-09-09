d100e8cdfe8f8fcd40adcdafa1a4c58da820be1a
Achmad Fatoni <achmadfatony@gmail.com>
2017-09-09 12:17:14 +0700

    displaying category items

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
     var baseUrl = 'https://re-inventor.herokuapp.com/api/';
     var categoriesUrl =  baseUrl + 'categories';
     var listCategoryElem = $('#list-category');
+    var listItem = $('#list-item');
 
-    $( "a" ).on( "click", function( event ) {
+    $("body").on( "click", "a", function( event ) {
         var page = $(this).attr( "href" );
 
         if (page == '#list-category-page') {
             loadCategories();
+        }
+
+        if (page == '#category-detail-page') {
+            var elem = $(event.target).closest('.detail');
+            loadCategoryDetail(elem.data('category-id'));
         }
     });
 
@@ -23,12 +29,48 @@ $(document).ready(function() {
 
                     var html ='<li data-role="list-divider">Category</li>';
                     $.each(json.categories, function(index, item) {
-                        html += '<li data-icon="gear"><a href="Category.html">' + item.name+ '</a></li>';
+                        html += '<li data-icon="gear" class="detail" data-category-id="' + item.id + '"><a href="#category-detail-page">' + item.name+ '</a></li>';
                     });
 
                     listCategoryElem.html($(html));
                     listCategoryElem.trigger('create');
                     listCategoryElem.listview('refresh');
+                }
+            },
+            error: function(xhr, desc, err) {
+                alert(err);
+                console.log(xhr + "\n" + err);
+            }
+        });
+    }
+
+    function loadCategoryDetail(id)
+    {
+        console.log('loading detail');
+        console.log(id);
+        $.ajax({
+            url: categoriesUrl + '/' + id,
+            type: 'GET',
+            cache: false,
+            success: function(json) {
+                console.log(json);
+                if (json.category) {
+                    console.log(json.category.name);
+                    $('#category-name').html(json.category.name);
+
+                    var html ='<li data-role="list-divider">List Item</li>';
+                    console.log(json.category.items.length);
+                    if (json.category.items.length){
+                        $.each(json.category.items, function(index, item) {
+                            html += '<li data-icon="gear" class="detail" data-category-id="' + item.id + '"><a href="#">' + item.name+ '</a></li>';
+                        });
+                    }else{
+                        html += '<li data-icon="gear"><a href="#"> No data</a></li>';
+                    }
+                    listItem.html($(html));
+                    listItem.trigger('create');
+                    listItem.listview('refresh');
+
                 }
             },
             error: function(xhr, desc, err) {
